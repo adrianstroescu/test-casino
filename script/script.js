71f0deb51544b60010eab1cbf43b1e059ae2e2db
Adrian21 <33807068+adrianstroescu@users.noreply.github.com>
2024-06-12 21:44:16 +0300

    test2

--- script/script.js
+++ script/script.js
@@ -1,18 +1,67 @@
 
 // Validation subscribe email
 
-        window.addEventListener('load', function () {
-            var forms = document.getElementsByClassName('needs-validation');
-            var validation = Array.prototype.filter.call(forms, function (form) {
-                form.addEventListener('submit', function (event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-                    form.classList.add('was-validated');
-                }, false);
-            });
+window.addEventListener('load', function () {
+    var forms = document.getElementsByClassName('needs-validation');
+    var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
         }, false);
+    });
+}, false);
 
-   
+// Tabel  Data   
+const tableData = [
+    {
+      paymentMethod: 'Master Card',
+      minDeposit: '£10',
+      maxWithdrawal: '£2500',
+      withdrawTime: '2-5 Days'
+    },
+    {
+      paymentMethod: 'VISA',
+      minDeposit: '£20',
+      maxWithdrawal: '£2500',
+      withdrawTime: '2-5 Days'
+    },
+    {
+      paymentMethod: 'Trustly',
+      minDeposit: '£10',
+      maxWithdrawal: '£2500',
+      withdrawTime: '24h'
+    },
+    {
+      paymentMethod: 'Skrill',
+      minDeposit: '£10',
+      maxWithdrawal: '£2500',
+      withdrawTime: '24h'
+    },
+    {
+      paymentMethod: 'NETELLER',
+      minDeposit: '£10',
+      maxWithdrawal: '£2500',
+      withdrawTime: '2-5 Days'
+    }
+  ];
 
+  // Function to populate the table with data
+function populateTable() {
+    const tableBody = document.getElementById('payment-table').getElementsByTagName('tbody')[0];
+    tableData.forEach((rowData) => {
+      const createTr = document.createElement('tr');
+      createTr.innerHTML = `
+        <td><img src="images/card.png" alt="Card Image">${rowData.paymentMethod}</td>
+        <td><span class="small">${rowData.minDeposit}</span></td>
+        <td>${rowData.maxWithdrawal}</td>
+        <td><span class="small">${rowData.withdrawTime}</span></td>
+      `;
+      tableBody.appendChild(createTr);
+    });
+  }
+  
+  
+  populateTable();
